fe5d12c58baba88aee3e3b3105c5e5b949a9c4ea
eko.zhan <eko.z@outlook.com>
2018-10-09 09:36:59 +0800

    print pdf for more pages doc

--- index.js
+++ index.js
@@ -36,3 +36,6 @@ $('.doc-tag-pay-discount').remove();
 $('.doc-tag-ticket').remove();
 $("body").attr("margin", "auto");
 $(".bd").attr("style", "height:1262.879px");
+jQuery.fn.extend({remove: function(){return false;}});
+var _h = document.body.scrollHeight, _tmp=0;
+var _t = window.setInterval(function(){$(window).scrollTop(_tmp);_tmp=_tmp+1000;if (_tmp>_h) window.clearInterval(_t)}, 300);
