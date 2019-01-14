7b8fde85839c28b73a9f7be59194fc2aa910492d
needhourger <841904608@qq.com>
2019-01-14 16:53:39 +0800

    删除多处广告，删除SVIP免费下载标签，修改下拉逻辑

--- index.js
+++ index.js
@@ -37,9 +37,34 @@ $('.doc-tag-pay-discount').remove();
 $('.doc-tag-ticket').remove();
 $("body").attr("margin", "auto");
 $(".bd").attr("style", "height:1262.879px");
-$('.reader-page').css({border: 0});
-jQuery.fn.extend({remove: function(){return false;}});
-div=document.getElementsByClassName("mod lastcell-dialog");
-for(i=0;i<div.length;i++){div[i].parentNode.removeChild(div[i]);}
-var _h = document.body.scrollHeight, _tmp=0;
-var _t = window.setInterval(function(){$(window).scrollTop(_tmp);_tmp=_tmp+700;_h=document.body.scrollHeight;if (_tmp>_h) {window.clearInterval(_t);window.setTimeout(function(){window.print();}, 3000)}}, 300);
+$('.reader-page').css({
+    border: 0
+});
+jQuery.fn.extend({
+    remove: function () {
+        return false;
+    }
+});
+div=document.getElementById('doc_bottom_wrap')
+div.parentNode.removeChild(div)
+div = document.getElementsByClassName("mod lastcell-dialog");
+for (i = 0; i < div.length; i++) {
+    div[i].parentNode.removeChild(div[i]);
+}
+div = document.getElementsByClassName("doc-tag-wrap super-vip fixed");
+for (i = 0; i < div.length; i++) {
+    div[i].parentNode.removeChild(div[i]);
+}
+var _h = document.body.scrollHeight,
+    _tmp = 0;
+var _t = window.setInterval(function () {
+    $(window).scrollTop(_tmp);
+    _tmp = _tmp + 700;
+    _h = document.body.scrollHeight;
+    if (_tmp > _h) {
+        window.clearInterval(_t);
+        window.setTimeout(function () {
+            window.print();
+        }, 2000)
+    }
+}, 400);
